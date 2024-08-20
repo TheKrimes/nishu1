1bed5b981177747dee18824b20e964201d857047
TheKrimes <101723485+TheKrimes@users.noreply.github.com>
2024-08-20 16:45:02 +0530

    Update Gif.js

--- Gif.js
+++ Gif.js
@@ -1,3 +1,13 @@
+   require('dotenv').config();
+   const { Client, GatewayIntentBits } = require('discord.js');
+   const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+   client.once('ready', () => {
+       console.log(`Logged in as ${client.user.tag}!`);
+   });
+   
+   client.login(process.env.DISCORD_BOT_TOKEN);
+
 const { Client, GatewayIntentBits } = require('discord.js');
 const axios = require('axios');
 
